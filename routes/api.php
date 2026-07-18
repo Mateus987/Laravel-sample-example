927d73eb4c1a07fa7f9ab56ebae2a5588a1d17ce
Mateus dos Santos de Andrade <63256486+Mateus987@users.noreply.github.com>
2026-07-17 23:25:27 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\PedidoController;
 Route::get('/pedidos', [PedidoController::class, 'index']);
 Route::post('/pedido', [PedidoController::class, 'store']);
 Route::put('/pedido', [PedidoController::class, 'store']);
+Route::delete()
